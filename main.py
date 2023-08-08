df5843eded45fd30e3c2a6d426beb2c2c72a82bc
Quinn Thorsnes <quinn.thorsnes@gmail.com>
2023-08-08 12:17:15 +1200

    added main, age sanitization
    This commit too big, sorry. Main loop, age as digits, end condition.

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-name = input("Please enter your name")
-age = int(input("Please enter your age"))
+
+def main():
+    satisfied = False
+    while not satisfied:
+        name = input("Please enter your name:\n")
+        age = ""
+        while not age:
+            try:
+                age = int(input("Please enter your age:\n"))
+            except:
+                print("Please enter age as digits.")
+        we_good = input("Your name is " + name + " and age is " + str(age) + " correct? y/n?\n")
+        we_good = we_good.lower()
+        if(we_good == "yes" or we_good == "yep" or we_good == "y"):
+            satisfied = True
+        else:
+            print("I'm sorry, let's try again.\n")
+
+main()
