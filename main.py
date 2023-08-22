601d216a6af62603cff44268404068805ddacb55
Quinn Thorsnes <quinn.thorsnes@gmail.com>
2023-08-22 15:22:29 +1200

    Separate saving to new file
    Also made header on new file

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from datetime import datetime
-
 user_data = "user_data.csv"
 
-def query_user_data():
-    #taking in user data of 5 questions - 
-    #food, water, sleep, stress, physical activity
-    #on a scale of 1 to 10
+
+# TODO 1-off get user name
+
+def query_user_data() -> list:
+    # taking in user data of 5 questions -
+    # food, water, sleep, stress, physical activity
+    # on a scale of 1 to 10
     data = []
     print("On the scale of 1 to 10, how would you rate the follow:")
     data.append(input("Food: "))
@@ -13,13 +15,25 @@ def query_user_data():
     data.append(input("Sleep: "))
     data.append(input("Stress: "))
     data.append(input("Physical activity: "))
+    return data
+
+
+def write_user_data(new_line: list):
     with open(user_data, "a") as my_file:
         line = str(datetime.today().date()) + ","
-        for response in data:
+        for response in new_line:
             line = line + response + ','
         line = line[:-1] + "\n"
         my_file.write(line)
     print("File saved!")
 
-query_user_data()
+
+try:
+    open(user_data, "r")
+except:
+    with open(user_data, "a") as new_file:
+        new_file.write("date, food, water, sleep, stress, activity\n")
+
+new_data = query_user_data()
+write_user_data(new_data)
 
