0b04df5dfe4848dd3d12dac27772510c419bb924
gitdatgitdat <141184949+gitdatgitdat@users.noreply.github.com>
2023-08-24 13:21:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+#Disabled pandas. Was upsetting Mac
+#import pandas as pd
+#import matplotlib.pyplot as plt
 from datetime import datetime
-user_data = "user_data.csv"
+user_data = "[username].csv"
 
 
 # TODO 1-off get user name
@@ -11,12 +12,32 @@ def query_user_data() -> list:
     # food, water, sleep, stress, physical activity
     # on a scale of 1 to 10
     data = []
-    print("On the scale of 1 to 10, how would you rate the follow:")
+    print("Welcome! May I please have your username?")
+    username = input("Username: ")
+    data.append(username)
+    print("On the scale of 1 to 10, how would you rate your eating habits?")
+    print("A 1 would indicate very poor eating habits.")
+    print("A 10 would indicate perfect eating habits.")
     data.append(input("Food: "))
+    print("On a scale of 1 to 10, how would you rate your water intake?")
+    print("A 1 would indicate not drinking enough water")
+    print("A 10 would indicate drinking the perfect amount of water")
     data.append(input("Water: "))
+    print("On a scale of 1 to 10, how would you rate your sleep?")
+    print("A 1 would indicate very poor sleep quality.")
+    print("A 10 would indicate perfect sleep quality.")
     data.append(input("Sleep: "))
+    print("On a scale of 1 to 10, how would you rate your stress?")
+    print("WARNING: The scale is inverted with this question.") 
+    print("Pleae pay close attention to your answer.")
+    print("A 1 would indicate very low levels of stress")
+    print("A 10 would indicate extreme levels of stress")
     data.append(input("Stress: "))
+    print("On a scale of 1 to 10, how would you rate your physical acvitivty?")
+    print("A 1 would indicate very low levels of physical activity.")
+    print("A 10 would indicate very high levels of physical activity.")
     data.append(input("Physical activity: "))
+    print("Wonderful! Please check in again tomorrow.")
     return data
 
 
@@ -39,6 +60,6 @@ except:
 new_data = query_user_data()
 write_user_data(new_data)
 
-time_series = pd.read_csv(user_data) # reload the saved data as a dataframe for analysis
-time_series.plot() #make a quick plot to view the data
-plt.show() 
+#time_series = pd.read_csv(user_data) # reload the saved data as a dataframe for analysis
+#time_series.plot() #make a quick plot to view the data
+#plt.show() 
