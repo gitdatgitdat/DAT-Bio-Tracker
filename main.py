db2fb5dcd58a362d17026d826ca280dce38b4a55
Quinn Thorsnes <quinn.thorsnes@gmail.com>
2023-09-09 17:04:49 +1200

    Show basic calendar functionality

--- main.py
+++ main.py
@@ -2,9 +2,13 @@
 import pandas as pd
 import Functions_for_user_data as fud
 import matplotlib.pyplot as plt
-from tkinter import *
-from tkinter import ttk
-
+from tkcalendar import Calendar #, DateEntry
+try:
+    import tkinter as tk
+    from tkinter import ttk
+except ImportError:
+    import Tkinter as tk
+    import ttk
 
 # TODO - main.py seems to get upset after third call
 
@@ -27,13 +31,26 @@ print(time_series)
 time_series.plot() #make a quick plot to view the data
 plt.show()
 
-#root = Tk()
-#root.title("DAT Biotracker")
 
-#mainframe = ttk.Frame(root, padding="3 3 12 12")
-#mainframe.grid(column = 0, row = 0, sticky =(N, W ,E, S))
-#root.columnconfigure(0, weight=1)
-#root.rowconfigure(0, weight =1)
+# this function is from the tkcalendar tutorial: https://github.com/j4321/tkcalendar#howtos
+def show_example_calendar():
+    top = tk.Toplevel(root)
+
+    cal = Calendar(top, selectmode='none')
+    date = cal.datetime.today() + cal.timedelta(days=2)
+    cal.calevent_create(date, 'Hello World', "message")
+    cal.calevent_create(date, 'Reminder 2', 'reminder')
+    cal.calevent_create(date + cal.timedelta(days=-2), 'Reminder 1', 'reminder')
+    cal.calevent_create(date + cal.timedelta(days=3), 'Message', 'message')
+
+    cal.tag_config('reminder', background='red', foreground='yellow')
+
+    cal.pack(fill="both", expand=True)
+    ttk.Label(top, text="Hover over the events.").pack()
 
 
-#root.mainloop()
+root = tk.Tk()
+root.title("DAT Biotracker")
+ttk.Button(root, text='Example calendar with events', command = show_example_calendar).pack(padx=10, pady=10)
+
+root.mainloop()
